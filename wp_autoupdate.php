941f540f07a8ab141b320c6ec025ec4c9d260ccf
Abid Omar <contact@omarabid.com>
2015-03-08 17:42:56 +0100

    added curlybrace

--- wp_autoupdate.php
+++ wp_autoupdate.php
@@ -1,133 +1,134 @@
 <?php
-if( !class_exists( wp_auto_update ) ):
-class wp_auto_update
-{
-    /**
-     * The plugin current version
-     * @var string
-     */
-    private $current_version;
+if( !class_exists( wp_auto_update ) ) {
+	class wp_auto_update
+	{
+		/**
+		 * The plugin current version
+		 * @var string
+		 */
+		private $current_version;
 
-    /**
-     * The plugin remote update path
-     * @var string
-     */
-    private $update_path;
+		/**
+		 * The plugin remote update path
+		 * @var string
+		 */
+		private $update_path;
 
-    /**
-     * Plugin Slug (plugin_directory/plugin_file.php)
-     * @var string
-     */
-    private $plugin_slug;
+		/**
+		 * Plugin Slug (plugin_directory/plugin_file.php)
+		 * @var string
+		 */
+		private $plugin_slug;
 
-    /**
-     * Plugin name (plugin_file)
-     * @var string
-     */
-    private $slug;
+		/**
+		 * Plugin name (plugin_file)
+		 * @var string
+		 */
+		private $slug;
 
-    /**
-     * Initialize a new instance of the WordPress Auto-Update class
-     * @param string $current_version
-     * @param string $update_path
-     * @param string $plugin_slug
-     */
-    public function __construct($current_version, $update_path, $plugin_slug)
-    {
-        // Set the class public variables
-        $this->current_version = $current_version;
-        $this->update_path = $update_path;
-        $this->plugin_slug = $plugin_slug;
-        list ($t1, $t2) = explode('/', $plugin_slug);
-        $this->slug = str_replace('.php', '', $t2);
+		/**
+		 * Initialize a new instance of the WordPress Auto-Update class
+		 * @param string $current_version
+		 * @param string $update_path
+		 * @param string $plugin_slug
+		 */
+		public function __construct($current_version, $update_path, $plugin_slug)
+		{
+			// Set the class public variables
+			$this->current_version = $current_version;
+			$this->update_path = $update_path;
+			$this->plugin_slug = $plugin_slug;
+			list ($t1, $t2) = explode('/', $plugin_slug);
+			$this->slug = str_replace('.php', '', $t2);
 
-        // define the alternative API for updating checking
-        add_filter('pre_set_site_transient_update_plugins', array(&$this, 'check_update'));
+			// define the alternative API for updating checking
+			add_filter('pre_set_site_transient_update_plugins', array(&$this, 'check_update'));
 
-        // Define the alternative response for information checking
-        add_filter('plugins_api', array(&$this, 'check_info'), 10, 3);
-    }
+			// Define the alternative response for information checking
+			add_filter('plugins_api', array(&$this, 'check_info'), 10, 3);
+		}
 
-    /**
-     * Add our self-hosted autoupdate plugin to the filter transient
-     *
-     * @param $transient
-     * @return object $ transient
-     */
-    public function check_update($transient)
-    {
-        if (empty($transient->checked)) {
-            return $transient;
-        }
+		/**
+		 * Add our self-hosted autoupdate plugin to the filter transient
+		 *
+		 * @param $transient
+		 * @return object $ transient
+		 */
+		public function check_update($transient)
+		{
+			if (empty($transient->checked)) {
+				return $transient;
+			}
 
-        // Get the remote version
-        $remote_version = $this->getRemote_version();
+			// Get the remote version
+			$remote_version = $this->getRemote_version();
 
-        // If a newer version is available, add the update
-        if (version_compare($this->current_version, $remote_version->new_version, '<')) {
-            $obj = new stdClass();
-            $obj->slug = $this->slug;
-            $obj->new_version = $remote_version->new_version;
-            $obj->url = $remote_version->url;
-            $obj->package = $remote_version->package;
-            $transient->response[$this->plugin_slug] = $obj;
-        }
-        return $transient;
-    }
+			// If a newer version is available, add the update
+			if (version_compare($this->current_version, $remote_version->new_version, '<')) {
+				$obj = new stdClass();
+				$obj->slug = $this->slug;
+				$obj->new_version = $remote_version->new_version;
+				$obj->url = $remote_version->url;
+				$obj->package = $remote_version->package;
+				$transient->response[$this->plugin_slug] = $obj;
+			}
+			return $transient;
+		}
 
-    /**
-     * Add our self-hosted description to the filter
-     *
-     * @param boolean $false
-     * @param array $action
-     * @param object $arg
-     * @return bool|object
-     */
-    public function check_info($false, $action, $arg)
-    {
-        if (isset($arg->slug) && $arg->slug === $this->slug) {
-            $information = $this->getRemote_information();
-            return $information;
-        }
-        return false;
-    }
+		/**
+		 * Add our self-hosted description to the filter
+		 *
+		 * @param boolean $false
+		 * @param array $action
+		 * @param object $arg
+		 * @return bool|object
+		 */
+		public function check_info($false, $action, $arg)
+		{
+			if (isset($arg->slug) && $arg->slug === $this->slug) {
+				$information = $this->getRemote_information();
+				return $information;
+			}
+			return false;
+		}
 
-    /**
-     * Return the remote version
-     * @return string $remote_version
-     */
-    private function getRemote_version()
-    {
-        $request = wp_remote_post($this->update_path, array('body' => array('action' => 'version')));
-        if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200) {
-            return unserialize( $request['body'] );
-        }
-        return false;
-    }
+		/**
+		 * Return the remote version
+		 * @return string $remote_version
+		 */
+		private function getRemote_version()
+		{
+			$request = wp_remote_post($this->update_path, array('body' => array('action' => 'version')));
+			if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200) {
+				return unserialize( $request['body'] );
+			}
+			return false;
+		}
 
-    /**
-     * Get information about the remote version
-     * @return bool|object
-     */
-    private function getRemote_information()
-    {
-        $request = wp_remote_post($this->update_path, array('body' => array('action' => 'info')));
-        if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200) {
-            return unserialize($request['body']);
-        }
-        return false;
-    }
+		/**
+		 * Get information about the remote version
+		 * @return bool|object
+		 */
+		private function getRemote_information()
+		{
+			$request = wp_remote_post($this->update_path, array('body' => array('action' => 'info')));
+			if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200) {
+				return unserialize($request['body']);
+			}
+			return false;
+		}
 
-    /**
-     * Return the status of the plugin licensing
-     * @return boolean $remote_license
-     */
-    private function getRemote_license()
-    {
-        $request = wp_remote_post($this->update_path, array('body' => array('action' => 'license')));
-        if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200) {
-            return $request['body'];
-        }
-        return false;
-    }
+		/**
+		 * Return the status of the plugin licensing
+		 * @return boolean $remote_license
+		 */
+		private function getRemote_license()
+		{
+			$request = wp_remote_post($this->update_path, array('body' => array('action' => 'license')));
+			if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200) {
+				return $request['body'];
+			}
+			return false;
+		}
+	}
 }
